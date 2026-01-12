a9feeea1d2d8343f5e267ec6971d0d2cbca14770
Danny <116danny.delaney@gmail.com>
2026-01-12 15:46:16 +0000

    npc

--- RumbleTumbleRobots/Scenes/red_actions.gd
+++ RumbleTumbleRobots/Scenes/red_actions.gd
@@ -2,12 +2,21 @@ extends CharacterBody2D
 
 @onready var sprite: AnimatedSprite2D = $Sprite
 
+var npc_mode := false  # false = player, true = npc
+
 var DODGE_DIST := 50
 
 var is_punching := false
 var is_dodging := false
 
+func _ready():
+	randomize()
+	npc_loop()
+
 func _process(_delta):
+	if npc_mode:
+		return
+
 	if not is_punching and not is_dodging:
 		if Input.is_action_just_pressed("Red_Punch"):
 			punch()
@@ -22,12 +31,30 @@ func dodge():
 	is_dodging = true
 
 	position.x += DODGE_DIST
-	await get_tree().create_timer(0.2).timeout
+	await get_tree().create_timer(0.5).timeout
 	position.x -= DODGE_DIST
 
 	is_dodging = false
-	
+
+func npc_loop():
+	while true:
+		await get_tree().create_timer(0.3).timeout  # npc think time
+		if not npc_mode:
+			continue
+		if is_punching or is_dodging:
+			continue
+
+		# 50/50 random choice
+		if randi() % 2 == 0:
+			punch()
+		else:
+			dodge()
+
 func _on_sprite_animation_finished():
 	if sprite.animation == "punch":
 		is_punching = false
 		sprite.play("idle")
+
+
+func _on_button_pressed() -> void:
+	npc_mode = !npc_mode
